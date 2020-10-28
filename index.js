fdae2572480c265aed38e210936d0768ff8b33b1
Kateryna Kochubei <kochubej.katerina@gmail.com>
2020-10-28 14:01:01 +0200

    corrected the conditions

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-// let array = [5, 3, 15, 20, 25, 10, 10, 12, 20, 25, 5, 3, 15, 20, 25,];
+let array = [5, 3, 15, 20, 25, 10, 10, 12, 20, 25, 5, 3, 15, 20, 25,];
 
-let array = [];
+// let array = [];
 
-function getRandomInt(min, max) {
-  let randomInt = Math.floor(Math.random() * (max-min)) + min;
-  return randomInt;
-}
+// function getRandomInt(min, max) {
+//   let randomInt = Math.floor(Math.random() * (max-min)) + min;
 
-function pushArray(max) {
-  while (array.length < max) {
-    array.push(getRandomInt(10, 20));
-  }
-  console.log(array);
-  return array
-} 
+//   return randomInt;
+// }
 
-pushArray(10);
+// function pushArray(max) {
+//   while (array.length < max) {
+//     array.push(getRandomInt(10, 20));
+//   }
+//   console.log(array);
+//   return array
+// } 
+
+// pushArray(10);
 
 function fooBarBazz(array, num) {
   for (i = 0; i < num; i++) {
     console.log(array[i])
     let element = array[i];
-    if (element % 3 === 0) {
+    if (element % 3 === 0 && element % 5 !== 0) {
       console.log('foo');
       alert('foo');
-    } else if (element % 5 === 0) {
+    } else if (element % 5 === 0 && element % 3 !== 0) {
       console.log('bar');
       alert('bar');
     } else if (element % 3 === 0 && element % 5 === 0) {
